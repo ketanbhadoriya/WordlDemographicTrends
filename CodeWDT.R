485a8725f31100a34a0ef5c0e585d4148230b291
ketansinghfmd <ketan.bhadoriya@gmail.com>
2018-06-09 21:54:59 +0530

    Second Commit

--- CodeWDT.R
+++ CodeWDT.R
@@ -1,1 +1,16 @@
 #Start of the Project
+
+#Reading the File
+#stringsAsFactors
+stats <- read.csv("DemographicData.csv")
+
+
+#Exploring the DataSet
+str(stats)
+
+head(stats)
+tail(stats)
+
+summary(stats)
+str(stats)
+levels(stats$Income.Group)
