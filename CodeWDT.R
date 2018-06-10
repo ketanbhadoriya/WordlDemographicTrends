952888e17bf3252881243bd51f9469387aa45014
ketansinghfmd <ketan.bhadoriya@gmail.com>
2018-06-10 12:20:31 +0530

    Final

--- CodeWDT.R
+++ CodeWDT.R
@@ -29,3 +29,23 @@ stats[stats$Birth.rate > 40 & stats$Internet.users <2,]
 #Visualizing the DataSet
 qplot(data=stats,x= Internet.users, y = Birth.rate, color = Income.Group,size=I(2))
 
+#Creating Dataframe
+my_df <- data.frame(Countries_2012_Dataset,Codes_2012_Dataset,Regions_2012_Dataset)
+head(my_df)
+
+#Changing the names of the Columns
+colnames(my_df) <- c("Country","Code","Region")
+head(my_df)
+
+#Merging the Dataframes
+head(stats)
+head(my_df)
+merged <- merge(stats,my_df,by.x = "Country.Code",by.y= "Code")
+head(merged)s
+merged$Country <- NULL
+str(merged)
+
+#Visualizing the DataSet Part 2
+qplot(data=merged,x= Internet.users, y = Birth.rate, color = Region,size=I(2),
+      shape=I(17),alpha=I(0.7),
+      main="Birth Rate Vs Internet Users")
